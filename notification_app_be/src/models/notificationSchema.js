11b646dd78bdc4703235f0bda70867c87b4bd8c9
Gajula Eshwarnath <gajulaeshwarnath24@gmail.com>
2026-05-06 15:58:29 +0530

    Add notification schema with table definitions

---
+++ notification_app_be/src/models/notificationSchema.js
@@ -0,0 +1,21 @@
+
+// CREATE TABLE Mail (
+//     id PRIMARY KEY,
+//     type CHARACTER(50),
+//     message CHARACTER(500),
+//     createdAt Date
+// )
+
+
+// CREATE TABLE Students (
+//     id PRIMARY KEY,
+//     name CHARACTER(50),
+//     email EMAIL 
+// )
+
+
+// CREATE TABLE Notifications (
+//     studentId FORGIEN KEY
+//     notificationID FORGIEN KEY
+//     isRead BOOLEAN
+// )
